f4ad6f4fd43558048376e13799edee6ed6a20ff2
jakuben <jakubfilipiak17@gmail.com>
2026-04-02 23:54:10 +0200

    jf-corrections-entites (remove bigDecimals)

--- src/main/java/com/example/demo/entities/Dish.java
+++ src/main/java/com/example/demo/entities/Dish.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Entity
@@ -17,7 +16,7 @@ public class Dish {
     @GeneratedValue
     private long id;
     private String name;
-    private BigDecimal price;
+    private double price;
     private String description;
 
     @JsonIgnore

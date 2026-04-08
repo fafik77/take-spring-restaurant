0fbd488477ee986ff82bf7bec9dc6df635f7ca03
fafik77 <55334002+fafik77@users.noreply.github.com>
2026-04-08 18:21:28 +0200

    removed legacy/example code (#6)
    Co-authored-by: Patryk Niesporek <pn312381@student.polsl.pl>

--- src/main/java/com/example/demo/controllers/CarController.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.controllers;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController //why RestController? It automatically applies @ResponseBody = JSON
-public class CarController {
-}
